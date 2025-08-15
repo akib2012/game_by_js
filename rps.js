0df25b6a9cef7d74abb1849b1718622189699abb
akib bhuiyan <akibbhuiyan3544@gmail.com>
2025-08-15 23:27:06 +0600

    addinting some new feature on this game

--- rps.js
+++ rps.js
@@ -1,48 +1,64 @@
 // Day 07 Learn To Build Beginner-Friendly JavaScript Projects 
 
-function rockPaperScissors(){
-    console.log("rock paper scissors");
-    const userChoisePromt = prompt("Enter rock ,paper and scissors");
+function rockPaperScissors() {
+    console.log("rock paper scissors GAME");
+    const userChoisePromt = prompt("Enter rock ,paper and scissors"); // by this window get a input feild 
     let userChoise = userChoisePromt.toLowerCase();
 
     let computerChoise;
     computerChoise = Math.floor(Math.random() * 3) + 1;
 
-    if(computerChoise === 1){
+    if (computerChoise === 1) {
         computerChoise = "rock";
 
     }
-    else if(computerChoise === 2){
+    else if (computerChoise === 2) {
         computerChoise = "paper";
     }
-    else{
+    else {
         computerChoise = "scissors";
     }
-    console.log("user selceted",userChoise);
+    console.log("user selceted", userChoise);
     console.log("computerb  selected", computerChoise);
 
-    if(
+    if (
         (userChoise === "rock" && computerChoise === "scissors") ||
-        (userChoise === "paper" && computerChoise === "rock")||
+        (userChoise === "paper" && computerChoise === "rock") ||
         (userChoise === "scissors" && computerChoise === "paper")
 
-    ){
-       console.log("TOU is WIN, YEAAA!!!"); 
+    ) {
+        console.log("TOU is WIN, YEAAA!!!");
     }
-    else if(userChoise === computerChoise){
+    else if (userChoise === computerChoise) {
         console.log("The game is TIE");
     }
-    else if(
+    else if (
         (userChoise === "rock" && computerChoise === "paper") ||
-        (userChoise === "paper" && computerChoise === "scissors")||
+        (userChoise === "paper" && computerChoise === "scissors") ||
         (userChoise === "scissors" && computerChoise === "rock")
 
-    ){
+    ) {
         console.log("computer Win!!");
     }
-    else{
+    else {
         console.log("please cheek the input this i do not understand!!");
     }
+
+    const PlayAgaimPromt = prompt("Do you wamt to play again? (yes/no)");
+    let PlayAgain;
+    if (PlayAgaimPromt) {
+        PlayAgain = PlayAgaimPromt.toLowerCase();
+    } else {
+        PlayAgain = "no";
+    }
+    if (PlayAgain === "yes") {
+        rockPaperScissors();
+
+
+    } else {
+        console.log("thank for playing game !! see you next time");
+    }
+
 
 
 }
